d219b1da4ba9c2e76987f27de51f48e5b0dc5f3d
bindhu97 <binduvishwakarma97@gmail.com>
2023-01-13 23:03:18 +0530

    [Bindhu]Add:User Entry by Lambda Expression

--- UserRegistration1/Program.cs
+++ UserRegistration1/Program.cs
@@ -3,5 +3,11 @@
 
 using UserRegistration1;
 
+List<ValidationResult> validEnteries = new List<ValidationResult>();
+
 RegistrationOfUser user = new RegistrationOfUser();
 user.UserRegistration();
+foreach (ValidationResult entry in validEnteries.FindAll(e => e.Result == false))
+{
+    Console.WriteLine("Invalid Entry : " + entry.UserEntry);
+}
---
+++ UserRegistration1/ValidationResult.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UserRegistration1
+{
+    public  class ValidationResult
+    {
+        public string UserEntry { get; set; }
+        public bool Result { get; set; }
+        public ValidationResult(string userEntry, bool result)
+        {
+            UserEntry = userEntry;
+            Result = result;
+        }
+    }
+}
